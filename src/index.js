fd7f7e07814eb1bb320ea2769e2b5e1163aefa97
Ben <brbryant2639@gmail.com>
2022-08-29 17:21:14 -0500

    UPDATE: new answer

--- src/index.js
+++ src/index.js
@@ -4,42 +4,56 @@ let form = document.querySelector("#submitForm");
 let makeInput = document.querySelector("#makeInput");
 let modelInput = document.querySelector("#modelInput");
 let yearInput = document.querySelector("#yearInput");
+let makeDisplay = document.querySelector("#car-make");
+let modelDisplay = document.querySelector("#car-model");
+let yearDisplay = document.querySelector("#car-year");
+let removeBtn = document.querySelector("#removeBtn");
 let wishlistUl = document.querySelector("#wishListContainer > ul");
 
 let wishlist = new WishList();
 
 form.addEventListener("submit", addCar);
 
-document.querySelector(".removeBtn").addEventListener("click", removeCar);
+removeBtn.addEventListener("click", removeCar);
 
 function updateDOMList() {
   wishlistUl.innerHTML = "";
   wishlist.list.forEach((car) => {
     const li = document.createElement("li");
     li.textContent = `${car.make} ${car.model}`;
-    li.addEventListener("click", () => showCarDetails(car))
+    li.addEventListener("click", () => showCarDetails(car));
     wishlistUl.appendChild(li);
-  })
+  });
 }
 
 function showCarDetails(car) {
-  // todo
+  makeDisplay.textContent = car.make;
+  modelDisplay.textContent = car.model;
+  yearDisplay.textContent = car.year;
+  removeBtn.disabled = false;
+  removeBtn.setAttribute("data-carId", car.id);
 }
 
 function addCar(event) {
-    event.preventDefault();
+  event.preventDefault();
 
   let make = makeInput.value;
   let model = modelInput.value;
   let year = yearInput.value;
 
   wishlist.add(make, model, year);
-  
+
   updateDOMList();
 }
 
-function removeCar(carId) {
-   wishlist.remove(carId);
-  
-   updateDOMList();
+function removeCar() {
+  let carId = Number(removeBtn.getAttribute("data-carId"));
+  wishlist.remove(carId);
+
+  updateDOMList();
+
+  makeDisplay.textContent = "";
+  modelDisplay.textContent = "";
+  yearDisplay.textContent = "";
+  removeBtn.disabled = true;
 }
